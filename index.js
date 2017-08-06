cf8f403fa7d18364ca5d7d5498eb569dfd3723da
Khab40 <alexey.khabalov@gmail.com>
2017-08-06 14:12:20 +0300

    improved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ buy: 'buddy' });
+  res.send({ hello: 'khab' });
 });
 
 const PORT = process.env.PORT || 5000;
