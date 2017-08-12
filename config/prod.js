46bae1e3571d3aa85b78bdafe0370d6d82ba7ad7
Khab40 <alexey.khabalov@gmail.com>
2017-08-12 21:01:55 +0300

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+// prod.js - production keys here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+//854391520454-gl7lu6r07unl0vc0joiiccqrodpb3sm0.apps.googleusercontent.com
+//DuiJ8jbDujRddVSLtu8Yxd9X
