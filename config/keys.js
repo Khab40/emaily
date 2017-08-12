f9970c69483c10fd54bd5d1b95483593eeae7685
Khab40 <alexey.khabalov@gmail.com>
2017-08-12 22:29:02 +0300

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV == 'production') {
   // we in production - return the prod set of keys
-  //854391520454-gl7lu6r07unl0vc0joiiccqrodpb3sm0.apps.googleusercontent.com
-  //DuiJ8jbDujRddVSLtu8Yxd9X
+  module.exports = require('./prod.js');
 } else {
   // we are in development - return dev keys
   module.exports = require('./dev.js');
