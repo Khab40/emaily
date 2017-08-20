ecd41a780a2732c5a57280dbdbe968cf570550be
Khab40 <alexey.khabalov@gmail.com>
2017-08-20 23:58:24 +0300

    Added billing and client side app

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
-
-//854391520454-gl7lu6r07unl0vc0joiiccqrodpb3sm0.apps.googleusercontent.com
-//DuiJ8jbDujRddVSLtu8Yxd9X
